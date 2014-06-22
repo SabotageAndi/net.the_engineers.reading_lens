02142826f9848bdd72ea1e0d96016ffe7568e801
Andreas Willich <sabotageandi@gmail.com>
2014-06-22 23:54:01 +0200

    add license

--- src/net/the_engineers/reading_lens/MainActivity.java
+++ src/net/the_engineers/reading_lens/MainActivity.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2014 Andreas Willich
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package net.the_engineers.reading_lens;
 
 import android.app.Activity;
@@ -40,7 +57,7 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback, Vi
                 _camera.autoFocus(new Camera.AutoFocusCallback() {
                     @Override
                     public void onAutoFocus(boolean success, Camera camera) {
-                        
+
                     }
                 });
             }
