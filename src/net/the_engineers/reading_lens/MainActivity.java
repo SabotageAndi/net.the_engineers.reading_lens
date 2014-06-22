da1571802733e0a6271e72643fa8a6e755945cb3
Andreas Willich <sabotageandi@gmail.com>
2014-06-22 22:12:18 +0200

    init commit

---
+++ src/net/the_engineers/reading_lens/MainActivity.java
@@ -0,0 +1,15 @@
+package net.the_engineers.reading_lens;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
